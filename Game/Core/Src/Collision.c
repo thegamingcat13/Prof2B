98e626d907831abd1d1b62cdabe0a5b2b35177eb
thegamingcat13 <sandervanbeek2004@gmail.com>
2026-01-26 11:58:24 +0100

    Comments

--- Game/Core/Src/Collision.c
+++ Game/Core/Src/Collision.c
@@ -11,12 +11,14 @@ bool Collision()
 	// Loop through all enemy's
 	for (int i = 0; i < MAX_ENEMYS; i++)
 	{
-		// Check if enemy lane is the same as the players
+		// Check if enemy lane is the same as the players and if the enemy is active
 		if (Enemy[i].isActive && Player.lane == Enemy[i].lane)
 		{
+			// temp variables to store the precise enemy location
 			int enemy_front = Enemy[i].yPosition + ENEMY_HEIGHT;
 			int enemy_back	= Enemy[i].yPosition;
 
+			// Compare enemy location to players
 			if (enemy_front >= PLAYER_TOP && enemy_back <= PLAYER_BOTTOM)
 				return true;
 		}
